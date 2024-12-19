6b91888460b71fb9298a1abe26873ff846bab292
Rajkumar Pambhar <rajkumarpambhar770@gmail.com>
2024-12-19 17:28:51 +0100

    Defined class CgiHandler

---
+++ includes/cgi/CgiHandler.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "RequestParser.hpp"
+#include "ClientState.hpp"
+
+class CgiHandler {
+	private:
+		RequestParser& request;
+		ClientState& clientState;
+
+		pid_t cgiPid;
+		int cgiStdinPipe[2];
+		int cgiStdoutPipe[2];
+		std::map<std::string, std::string> envVars;
+
+	public:
+		CgiHandler(RequestParser& req, ClientState& cs);
+		~CgiHandler();
+
+		void prepareEnvironment();
+		void executeCgi(const std::string &cgiScriptPath);
+		void writeToCgiStdin();
+		void readFromCgiStdout();
+		bool isCgiFinished();
+		void cleanup();
+};
